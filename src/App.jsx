c669d148a35dc4113e86931b1b8286501dd14edc
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-10-06 19:21:57 -0400

    Feat: add about option, command shorcuts and remove dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { FaDiscord, FaGithub } from 'react-icons/fa';
 import './App.css';
 
 export default function App() {
@@ -7,38 +6,50 @@ export default function App() {
 
     //commands and content
     const commands = {
+        "about": `I'm a self-taught FrontEnd developer from Venezuela 🇻🇪.\n I have interest and some experience with React.js, version control wiht Git, a so on.\n Also, I'm a computer science student from the Alexander of Humboldt University and I'm excited to learn more about coding in frontend and backend development.`,
         "help": [
             'there are several commands that can be used.',
             ' ',
-            'help          display this help message',
-            'version       display this version information',
-            'social       display social information like email addresses, github and so on'
+            'about -a        display about message to know about me',
+            'help -h         display this help message',
+            'version -v      display this version information',
+            'social -s       display social information like email addresses, github and so on'
         ],
         "version": [
             '~Portfolio 1.0.0~',
         ],
-        "social": [
-            'You can follow me on the following sites:',
-            `Github `,
-            `Dicord`
-        ]
+        "social": {
+            "github": "https://github.com/santiagohernandezs",
+            "discord": "https://discordapp.com/users/979726706069880913/",
+            "instagram": "https://www.instagram.com/taox.rar/",
+        }
     }
-
-    //setShow(commands.help.map(command => <ul><li>{command}</li></ul>))
 
     //this function allows to catch the enter key press
     const handleKey = e => {
         if (e.key === 'Enter') {
             let cmd = document.getElementById('cmd')
             switch (cmd.value) {
+                case 'about':
+                case '-a':
+                    setShow(commands.about)
+                    break
                 case 'help':
+                case '-h':
                     setShow(commands.help.map(command => <ul><li>{command}</li></ul>))
                     break;
                 case 'version':
+                case '-v':
                     setShow(commands.version)
                     break;
                 case 'social':
-                    setShow(commands.social.map(social => <ul><li>{social}</li></ul>))
+                case '-s':
+                    setShow(Object.entries(commands.social).map(pair => {
+                        const name = pair[0]
+                        const url = pair[1]
+                        return <ul><li>{name + '  =>  '}<a href={url}>{url}</a></li></ul>
+                    }))
+                    break;
             }
             e.preventDefault();
         }
