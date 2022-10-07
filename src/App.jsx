dd7aec1672c26bcbd47f9b971cce33abb8471d0b
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-10-06 20:43:20 -0400

    Refactor: change "help" option

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import './App.css';
 export default function App() {
     const [show, setShow] = useState('')
 
+    //'there are several commands that can be used.',
+    //' ',
     //commands and content
     const commands = {
-        "about": `I'm a self-taught FrontEnd developer from Venezuela 🇻🇪.\n I have interest and some experience with React.js, version control wiht Git, a so on.\n Also, I'm a computer science student from the Alexander of Humboldt University and I'm excited to learn more about coding in frontend and backend development.`,
-        "help": [
-            'there are several commands that can be used.',
-            ' ',
-            'about -a        display about message to know about me',
-            'help -h         display this help message',
-            'version -v      display this version information',
-            'social -s       display social information like email addresses, github and so on'
-        ],
+        "about": `I'm a self-taught FrontEnd developer from Venezuela 🇻🇪. I have interest and some experience with React.js, version control wiht Git, a so on. Also, I'm a computer science student from the Alexander of Humboldt University and I'm excited to learn more about coding in frontend and backend development.`,
+        "help": {
+            "about -a": "  display about message to know about me",
+            "help -h": "   display this help message",
+            "version -v": "display this version information",
+            "social -s": " display social information like email addresses, github and so on"
+        },
         "version": [
             '~Portfolio 1.0.0~',
         ],
@@ -36,7 +36,16 @@ export default function App() {
                     break
                 case 'help':
                 case '-h':
-                    setShow(commands.help.map(command => <ul><li>{command}</li></ul>))
+                    setShow(
+                        Object.entries(commands.help).map(pair => {
+                            const name = pair[0]
+                            const des = pair[1]
+                            return (
+                                <div>
+                                    <ul><li>{name + '    ' + des}</li></ul>
+                                </div>
+                            )
+                        }))
                     break;
                 case 'version':
                 case '-v':
@@ -58,9 +67,14 @@ export default function App() {
     return (
         <div className="App">
             <div className="wrapper">
-                <p className="welcome">Welcome to TAO's Portfolio feel free to explore in this terminal</p>
-                <textarea name="textarea" id="cmd" cols="30" rows="1" onKeyDown={handleKey} autofocus></textarea>
-                <div className="display">{show}</div>
+                <p className="welcome">
+                    Welcome to Tao's Portfolio feel free to explore in this terminal <br /> Copyright (C) Tao's Property. All rigths reserved.
+                </p>
+                <div className="command-line">
+                    <span>C:/User/Tao ~</span>
+                    <textarea name="textarea" id="cmd" cols="30" rows="1" onKeyDown={handleKey} autofocus></textarea>
+                </div>
+                <div className="display"><text>{show}</text></div>
             </div>
         </div>
     )
